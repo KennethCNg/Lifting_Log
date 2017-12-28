b46c91ebba8558dbff1881e5b0387eda39670ef2
Kenneth Ng <kenneth.ng.4@gmail.com>
2017-12-28 18:17:27 -0500

    add util calls

--- frontend/lifting.jsx
+++ frontend/lifting.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class Root extends React.Component {
+  render() {
+    return(
+      <div>
+        Welcome To Lifting
+      </div>
+    );
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-  const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to Lifting</h1>, root);
+  ReactDOM.render(<Root/>, document.getElementById('root'));
 });
