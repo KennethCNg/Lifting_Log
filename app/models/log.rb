8f27a046e605eb21d78d873c02d78ae736fa9a19
Kenneth Ng <kenneth.ng.4@gmail.com>
2017-12-28 17:19:08 -0500

    finish models

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,2 +1,25 @@
+# == Schema Information
+#
+# Table name: logs
+#
+#  id         :integer          not null, primary key
+#  weight     :integer          not null
+#  carb       :integer          not null
+#  fat        :integer          not null
+#  protein    :integer          not null
+#  author_id  :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Log < ApplicationRecord
+    validates :weight, :carb, :fat, :protein, :author_id, presence: true
+  
+    belongs_to(
+    :author,
+    primary_key: :id,
+    foreign_key: :author_id,
+    class_name: 'User'
+    )
+
 end
--- test/models/log_test.rb
+++ test/models/log_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: logs
+#
+#  id         :integer          not null, primary key
+#  weight     :integer          not null
+#  carb       :integer          not null
+#  fat        :integer          not null
+#  protein    :integer          not null
+#  author_id  :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class LogTest < ActiveSupport::TestCase
