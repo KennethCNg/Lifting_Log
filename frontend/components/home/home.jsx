bc1e2b1e7668347c46ff6aaaec15ab60f8f46752
Kenneth Ng <kenneth.ng.4@gmail.com>
2018-04-02 19:45:42 -0400

    make log index back into stateful component

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -3,8 +3,7 @@ import { Route, Link, NavLink } from  'react-router-dom';
 import { Grid, Form, FormControl, Button, Col, Row } from 'react-bootstrap';
 import merge from 'lodash';
 import LogFormContainer from '../log_form/log_form_container';
-import LogIndex from '../log_form_index/log_index';
-
+import LogIndexContainer from '../log_form_index/log_index_container';
 
 class Home extends React.Component {
   constructor(props) {
@@ -53,7 +52,7 @@ class Home extends React.Component {
           <Col xs={0} s={0} md={2} lg={2} xl={2}/>
           <Col xs={0} s={0} md={6} lg={6} xl={6}>
             { this.getDate() }
-            <LogIndex />
+            <LogIndexContainer />
           </Col>
         </Row> 
 
---
+++ frontend/components/log_form_index/log_index_container.jsx
@@ -0,0 +1,28 @@
+import { connect } from 'react-redux';
+import { logout } from '../../actions/session_actions';
+import { selectLogs } from '../../selectors/log_selector';
+import { requestAllLogs, requestCreateLog, requestDestroyLog } from '../../actions/log_actions';
+import LogIndex from './log_index';
+
+const mapStateToProps = state => {
+    debugger;
+  return {
+    logs: selectLogs(state),
+  };
+  
+};
+
+const mapDispatchToProps = dispatch => {
+    debugger;
+  return {
+    requestLogout: () => dispatch(logout()),
+    requestAllLogs: () => dispatch(requestAllLogs()),
+    requestCreateLog: (log) => dispatch(requestCreateLog(log)),
+    requestDestroyLog: (logId) => dispatch(requestDestroyLog(logId)),
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(LogIndex);
